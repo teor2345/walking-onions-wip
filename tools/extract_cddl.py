40e02761356327959b132b328d257fe321b36c6f
Nick Mathewson <nickm@torproject.org>
2020-03-30 09:10:20 -0400

    Teach extract_cddl.py to use ruby cddl implementation to check our cddl

--- tools/extract_cddl.py
+++ tools/extract_cddl.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 
+import argparse
+import re
+import subprocess
 import sys
+import tempfile
+
+def augmented_open(fname, mode="r"):
+    if fname == '-':
+        if 'w' in mode:
+            return sys.stdout
+        else:
+            return sys.stdin
+    return open(fname, mode)
+
+def warn(s):
+    sys.stderr.write(s)
 
 def extract_cddl_lines(f):
     """
@@ -20,16 +35,52 @@ def extract_cddl_lines(f):
                 in_cddl = True
                 yield line
 
+def strip_directives_for_ruby_cddl(s):
+    """
+    Remove directives that the ruby cddl implementation doesn't understand.
+    """
+    s = s.replace("bstr .cbor ", "").replace("encoded-cbor .cbor ", "")
+    return re.sub(r'.within \w+', '', s)
 
+def main(argv):
+    progname = argv[0]
+    parser = argparse.ArgumentParser(prog=progname)
+    parser.add_argument("--check", action="store_true",
+                        help="use a cddl implementation to check the scripts")
+    parser.add_argument("--toplevel",
+                        help="treat given symbol as top-level CDDL production")
+    parser.add_argument("--output", default="-",
+                        help="where to write [- for stdout]")
+    parser.add_argument("input", default=["-"], nargs="*",
+                        help="files to read from [- for stdin]")
+
+    args = parser.parse_args(argv[1:])
+
+    if args.output != '-' and args.check:
+        warn("Can't use --check and --output together.")
+        sys.exit(1)
+
+    if args.check:
+        outf = tempfile.NamedTemporaryFile(delete=False, mode='w', encoding='utf-8')
+        filt = strip_directives_for_ruby_cddl
+    else:
+        outf = augmented_open(args.output, 'w')
+        filt = lambda x: x
+
+    if args.toplevel:
+        print("TOPLEVEL_ = %s\n"%args.toplevel, file=outf)
+
+    try:
+        for fname in args.input:
+            with augmented_open(fname) as inf:
+                for line in extract_cddl_lines(inf):
+                    outf.write(filt(line))
+    finally:
+        outf.close()
+
+    if args.check:
+        subprocess.run(["cddl", outf.name, "generate"])
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] != '-':
-        stream = open(sys.argv[1], 'r')
-    else:
-        print("reading from stdin...", file=sys.stderr)
-        stream = sys.stdin
+    main(sys.argv)
 
-    for line in extract_cddl_lines(stream):
-        sys.stdout.write(line)
-
-    
